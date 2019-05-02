d605d3e3f19d408d34b052907a49e9fed5c29be0
Matt Wiens <mwiens91@gmail.com>
2019-05-01 21:04:01 -0700

    Remove Tableau class and just provide functions

--- tucker_simplepy/tableau.py
+++ tucker_simplepy/tableau.py
@@ -1,74 +1,106 @@
-"""Contains Tucker tableau class."""
+"""Contains helpers for applying the simplex algorithm to a Tucker tableau.
 
-from __future__ import annotations
+Tucker tableaus are represented by NumPy matrices (and use 0-indexing by
+default unless you specify otherwise). All of these functions treat
+matrices as immutable; i.e., they will not modify the matrices passed in
+and will return new matrices.
+
+The typing annotations in this module require Python >= 3.5.
+"""
+
 import itertools
 from typing import List, Tuple
 import numpy as np
 
 
-class Tableau:
-    """A Tucker tableau.
+def _get_all_coordinates(matrix: np.matrix) -> List[Tuple[int]]:
+    """Return cartesian product of matrix dimensions.
 
-    TODO: finish docstring
+    Args:
+        matrix: A NumPy matrix representing a tableau.
     """
+    n, m = matrix.shape
 
-    def __init__(self, matrix: np.matrix):
-        """TODO: docstring."""
-        self.matrix = matrix
-        self.n, self.m = matrix.shape
+    return list(itertools.product(list(range(n)), list(range(m))))
 
-    def get_all_coordinates(self) -> List[Tuple[int]]:
-        """Return cartesian product of tableau dimensions."""
-        return list(
-            itertools.product(list(range(self.n)), list(range(self.m)))
-        )
 
-    def pivot(self, i: int, j: int) -> Tableau:
-        """Pivot the tableau on entry with row i, column j.
+def remove_row(
+    matrix: np.matrix, i: int, index_by_1: bool = False
+) -> np.matrix:
+    """Remove the ith row from a matrix.
 
-        TODO finish docstring
-        """
-        # Initialize the pivoted matrix
-        n = np.zeros((self.n, self.m))
+    Args:
+        matrix: A NumPy matrix representing a tableau.
+        i: The row to remove.
+        index_by_1: Whether to index by 1 (cf. indexing by 0).
 
-        # Pivot. TODO: protect against zero division
-        for row, col in self.get_all_coordinates():
-            if (row, col) == (i, j):
-                # Pivot entry
-                n[row, col] = 1 / self.matrix[row, col]
-            elif row == i:
-                # Pivot row
-                n[row, col] = self.matrix[row, col] / self.matrix[i, j]
-            elif col == j:
-                # Pivot column
-                n[row, col] = -self.matrix[row, col] / self.matrix[i, j]
-            else:
-                n[row, col] = (
-                    self.matrix[row, col] * self.matrix[i, j]
-                    - self.matrix[row, j] * self.matrix[i, col]
-                ) / self.matrix[i, j]
+    Returns:
+        A matrix corresponding to the tableau with the ith row removed.
+    """
+    # Handle possible 1-indexing
+    if index_by_1:
+        i += 1
 
-        return Tableau(n)
+    return np.delete(matrix, (i), axis=0)
 
-    def remove_col(self, j: int) -> Tableau:
-        """Remove the jth column.
 
-        TODO finish docstring
-        """
-        new_matrix = np.delete(self.matrix, (j), axis=1)
+def remove_col(
+    matrix: np.matrix, j: int, index_by_1: bool = False
+) -> np.matrix:
+    """Remove the jth column from a matrix.
 
-        return Tableau(new_matrix)
+    Args:
+        matrix: A NumPy matrix representing a tableau.
+        j: The column to remove.
+        index_by_1: Whether to index by 1 (cf. indexing by 0).
 
-    def remove_row(self, i: int) -> Tableau:
-        """Remove the ith row.
+    Returns:
+        A matrix corresponding to the tableau with the jth column
+        removed.
+    """
+    # Handle possible 1-indexing
+    if index_by_1:
+        j += 1
 
-        TODO finish docstring
-        """
-        new_matrix = np.delete(self.matrix, (i), axis=0)
+    return np.delete(matrix, (j), axis=1)
 
-        return Tableau(new_matrix)
 
-    def __str__(self):
-        """Display the tableau nicely."""
-        # TODO print it more nicely
-        return str(self.matrix)
+def pivot(
+    matrix: np.matrix, i: int, j: int, index_by_1: bool = False
+) -> np.matrix:
+    """Pivot the tableau on entry with row i, column j.
+
+    Args:
+        matrix: A NumPy matrix representing a tableau.
+        i: The row to pivot on.
+        j: The column to pivot on.
+        index_by_1: Whether to index by 1 (cf. indexing by 0).
+
+    Returns:
+        A matrix corresponding to the tableau after pivoting on (i, j).
+    """
+    # Handle possible 1-indexing
+    if index_by_1:
+        i, j = (i + 1, j + 1)
+
+    # Initialize the pivoted matrix
+    output = np.zeros(matrix.shape)
+
+    # Pivot
+    for row, col in _get_all_coordinates(matrix):
+        if (row, col) == (i, j):
+            # Pivot entry
+            output[row, col] = 1 / matrix[row, col]
+        elif row == i:
+            # Pivot row
+            output[row, col] = matrix[row, col] / matrix[i, j]
+        elif col == j:
+            # Pivot column
+            output[row, col] = -matrix[row, col] / matrix[i, j]
+        else:
+            output[row, col] = (
+                matrix[row, col] * matrix[i, j]
+                - matrix[row, j] * matrix[i, col]
+            ) / matrix[i, j]
+
+    return output
